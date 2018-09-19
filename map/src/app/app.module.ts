01e3eba3b787cceb826884c5b8d586ad22eeacb4
WilliamBZA <william.brander@gmail.com>
2018-09-19 12:18:14 +0200

    Add map control component

--- map/src/app/app.module.ts
+++ map/src/app/app.module.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../node_modules/bingmaps/types/MicrosoftMaps/Microsoft.Maps.All.d.ts" />
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
@@ -5,15 +7,18 @@ import { createCustomElement } from '@angular/elements';
 import { AppComponent } from './app.component';
 import { MapControlComponent } from './map-control/map-control.component';
 
+import { MapModule, MapAPILoader, BingMapAPILoaderConfig, BingMapAPILoader, WindowRef, DocumentRef } from 'angular-maps';
+
 @NgModule({
   declarations: [
     AppComponent,
     MapControlComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MapModule.forRoot()
   ],
-  providers: [],
+  providers: [{ provide: MapAPILoader, deps: [], useFactory: MapServiceProviderFactory }],
   bootstrap: [AppComponent],
   entryComponents: [MapControlComponent]
 })
@@ -24,4 +29,14 @@ export class AppModule {
     const el = createCustomElement(MapControlComponent, { injector: this.injector });
     customElements.define('map-control', el);
   }
+}
+
+export function MapServiceProviderFactory(){
+  let bc: BingMapAPILoaderConfig = new BingMapAPILoaderConfig();
+  bc.apiKey ="AkO1rARMw-tWnH2pu_4ujk58o8KOmfscCaavb5OJngpLVrgNj3DVZZjLFyJxu4DZ"; // your bing map key
+  bc.branch = "experimental"; 
+      // to use the experimental bing brach. There are some bug fixes for
+      // clustering in that branch you will need if you want to use 
+      // clustering.
+  return new BingMapAPILoader(bc, new WindowRef(), new DocumentRef());
 }
--- map/src/app/map-control/map-control.component.ts
+++ map/src/app/map-control/map-control.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
+import { IBox, IMapOptions, ILatLong } from 'angular-maps';
 
 @Component({
   selector: 'app-map-control',
@@ -7,12 +8,31 @@ import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
   encapsulation: ViewEncapsulation.Native
 })
 export class MapControlComponent implements OnInit {
+  _options: IMapOptions = {
+    disableBirdseye: false,
+    disableStreetside: false,
+    navigationBarMode: 1, 
+    zoom: 6
+};
 
-  public huzzaa : string = 'Meow';
+public _path: Array<ILatLong> = new Array<ILatLong>();
 
-  constructor() { }
+constructor() {
+  PathData.forEach(element => {
+    this._path.push({ latitude: element.latitude, longitude: element.longitude});
+  });
+}
 
   ngOnInit() {
   }
-
 }
+
+const PathData = [
+  { 'latitude': 54.415767, 'longitude': 18.664087 },
+  { 'latitude': 58.403351, 'longitude': 21.095825 },
+  { 'latitude': 59.610423, 'longitude': 22.861209 },
+  { 'latitude': 60.137442, 'longitude': 24.941648 },
+  { 'latitude': 59.920115, 'longitude': 27.292766 },
+  { 'latitude': 60.069910, 'longitude': 28.835464 },
+  { 'latitude': 59.945509, 'longitude': 30.074524 },
+];
